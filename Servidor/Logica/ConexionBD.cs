67bd15c62e0b9fccd9fdf2c79ac7254775a70ff8
Asus <Asus@DESKTOP-I3RVQVB>
2025-06-22 19:08:13 -0500

    Agregar archivos de proyecto.

---
+++ Servidor/Logica/ConexionBD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servidor.Logica
+{
+    public static class ConexionBD
+    {
+        public static string Cadena = "Data Source=DESKTOP-I3RVQVB\\SQLEXPRESS04;Initial Catalog=Banco;Integrated Security=True;TrustServerCertificate=True";
+    }
+
+}
